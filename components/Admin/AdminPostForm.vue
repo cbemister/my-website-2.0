4f1286fbbbbb1e2ceb4797cc242b573c74b7b585
Chris <cbemister@autocan.ca>
2018-10-18 13:35:08 -0600

    Updated admin post form

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,13 +2,21 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+    <AppControlInput
+      v-model="editedPost.shortDescription">Short Description</AppControlInput>
+    <label for="featured">Category: </label>
+    <select name="category" v-model="editedPost.category">
+      <option value="post" selected>Post</option>
+      <option value="page">Page</option>
+      <option value="thought">Thought</option>
+    </select>
+    <label for="featured">Featured: </label>
+    <input v-model="editedPost.featured" type="checkbox" id="featured "name="featured" value="false">
     <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput
       control-type="textarea"
       v-model="editedPost.content">Content</AppControlInput>
-    <AppControlInput
-      control-type="textarea"
-      v-model="editedPost.previewText">Preview Text</AppControlInput>
+
     <AppButton type="submit">Save</AppButton>
     <AppButton
       type="button"
@@ -31,11 +39,13 @@ export default {
       editedPost: this.post
         ? { ...this.post }
         : {
-            author: "",
+            author: "Chris Bemister",
             title: "",
+            category: "",
+            featured: false,
             thumbnail: "",
             content: "",
-            previewText: ""
+            shortDescription: ""
           }
     };
   },
@@ -51,3 +61,11 @@ export default {
   }
 };
 </script>
+
+<style>
+
+  label {
+    font-weight: bold;
+  }
+
+</style>
