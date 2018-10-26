974d93e0b72fb1fb2de7eca08963ab124ff1c85f
Chris <cbemister@autocan.ca>
2018-10-26 13:22:00 -0600

    Set-up thank you page and tweaked form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,16 +1,37 @@
-
 <template>
-
-    <form name="contact" netlify>
-    <p>
-        <label>Name <input type="text" name="name" /></label>
-    </p>
-    <p>
-        <label>Email <input type="email" name="email" /></label>
-    </p>
-    <p>
-        <button type="submit">Send</button>
-    </p>
-    </form>
-
+  <div class="container">
+    <h1 class="title">
+      Contact
+    </h1>
+    <div class="content">
+      <form name="contact" name="contact-page" netlify-honeypot="bot-field" action="thank-you" method="post" netlify>
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="email">
+          Email:
+        </label>
+        <input class="form-field" name="email" id="email" />
+        <label class="form-label" for="message">
+          Message:
+        </label>
+        <textarea class="form-field" name="message" id="message"></textarea>
+        <input class="form-button" type="submit" value="Send message" />
+        <p class="hidden">     
+            <label>Don’t fill this out: 
+                <input name="bot-field">
+            </label>
+        </p>
+      </form>
+    </div>
+  </div>
 </template>
+
+<style>
+
+.hidden {
+  display: none;
+}
+
+</style>
