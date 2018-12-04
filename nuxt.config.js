2c924c69e8d44ac40ab2a5a654faf780981733e9
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-12-04 12:05:58 -0700

    Added sizes to link tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,9 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "apple-mobile-web-app-capable", content: "yes" },
       { name: "apple-mobile-web-app-title", content: "My Website" },
-      { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+      { name: "apple-mobile-web-app-status-bar-style", content: "default" },
       {
         hid: "description",
         name: "description",
@@ -22,13 +23,13 @@ module.exports = {
       }
     ],
     link: [
-      { rel: "apple-touch-startup-image", media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", href: "/apple-launch-1125x2436.png" },
-      { rel: "apple-touch-startup-image", media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-750x1334.png" },
-      { rel: "apple-touch-startup-image", media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)", href: "/apple-launch-1242x2208.png" },
-      { rel: "apple-touch-startup-image", media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-640x1136.png" },
-      { rel: "apple-touch-startup-image", media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-1536x2048.png" },
-      { rel: "apple-touch-startup-image", media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-1668x2224.png" },
-      { rel: "apple-touch-startup-image", media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-2048x2732.png" },
+      { rel: "apple-touch-startup-image", sizes: "1125x2436", media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", href: "/apple-launch-1125x2436.png" },
+      { rel: "apple-touch-startup-image", sizes: "750x1334", media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-750x1334.png" },
+      { rel: "apple-touch-startup-image", sizes: "1242x2208", media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)", href: "/apple-launch-1242x2208.png" },
+      { rel: "apple-touch-startup-image", sizes: "640x1136", media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-640x1136.png" },
+      { rel: "apple-touch-startup-image", sizes: "1536x2048", media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-1536x2048.png" },
+      { rel: "apple-touch-startup-image", sizes: "1668x2224", media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-1668x2224.png" },
+      { rel: "apple-touch-startup-image", sizes: "2048x2732", media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)", href: "/apple-launch-2048x2732.png" },
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
         rel: "stylesheet",
